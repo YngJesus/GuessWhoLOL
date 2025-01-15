6c00bd0de31500e393c21597b5d0b67b3122d5cc
Baha Eddine Jdidi <91160011+YngJesus@users.noreply.github.com>
2025-01-15 19:45:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// filepath: /c:/Users/bahaj/Desktop/GuessWhoLOL/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/GuessWhoLOL/", // Ensure this line is added
+  base: "/GuessWhoLOL/", 
 });
 
