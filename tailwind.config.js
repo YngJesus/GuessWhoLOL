a0090405b6a1ebce07f11ae34440aad870c516ba
Baha Eddine Jdidi <91160011+YngJesus@users.noreply.github.com>
2025-01-15 19:45:53 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", // Add this if you have an HTML file
-    "./src/**/*.{js,jsx,ts,tsx}", // Add paths to all your JS/JSX/TS/TSX files
+    "./index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
     extend: {},
