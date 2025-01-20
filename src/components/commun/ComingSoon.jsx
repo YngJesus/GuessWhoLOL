b8e31bb6570cb6fa1792411f51d0df7846264fc7
Baha Jdidi <91160011+YngJesus@users.noreply.github.com>
2025-01-20 20:29:58 +0100

    Update ComingSoon component with loading animation and adjust hero section height

--- src/components/commun/ComingSoon.jsx
+++ src/components/commun/ComingSoon.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { grid } from "ldrs";
+
+grid.register();
 
 function ComingSoon() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const video = document.getElementById("coming-soon-video");
+    video.onloadeddata = () => {
+      setLoading(false);
+    };
+  }, []);
+
   return (
-    <section>
-      {" "}
+    <section className="relative w-full h-screen overflow-hidden">
+      {loading && (
+        <div className="flex justify-center items-center h-screen">
+          <l-grid size="150" speed="1.5" color="white"></l-grid>
+        </div>
+      )}
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        id="coming-soon-video"
+        className={`absolute top-0 left-0 w-full h-full object-cover ${
+          loading ? "hidden" : ""
+        }`}
         src="/GuessWhoLOL/video/comingsoon.mp4"
         autoPlay
         loop
         muted
       ></video>
-      <div className="cs relative  flex flex-col justify-center items-center h-screen text-white">
-        COMING SOON
+      <div
+        className={`cs relative flex flex-col justify-center items-center h-screen text-white ${
+          loading ? "hidden" : ""
+        }`}
+      >
+        COMING SOON...
         <Link to="/" className="custom-btn1">
           Go Back!
         </Link>
