78d76b8260d1a6be5bde9d39f4dd4bcf4adb85ef
Baha Jdidi <91160011+YngJesus@users.noreply.github.com>
2025-02-01 20:39:22 +0100

    Update video source path in HeroSection component for correct file reference

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -32,7 +32,7 @@ function HeroSection() {
         <video
           id="background-video"
           className="absolute top-0 left-0 w-full h-full object-cover"
-          src="/GuessWhoLOL/video/jinx2.mp4"
+          src="/video/jinx2.mp4"
           autoPlay
           loop
           muted
