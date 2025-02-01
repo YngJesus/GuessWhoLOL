b8ab4935445fb37d7fe45a51d43b1f322b8c1c2b
Baha Jdidi <91160011+YngJesus@users.noreply.github.com>
2025-02-01 21:45:49 +0100

    Add Google AdSense integration to HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Navbar from "../components/commun/Navbar";
+import React, { useEffect } from "react";
 import HeroSection from "../components/Home/HeroSection";
 
 function HomePage() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense error:", e);
+    }
+  }, []);
+
   return (
     <>
       <HeroSection />
+
+      {/* Google AdSense Ad */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", textAlign: "center" }}
+        data-ad-client="ca-pub-9697028383298359"
+        data-ad-slot="4168013160"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
     </>
   );
 }
